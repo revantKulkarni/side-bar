719fdd6f7611d5ee0fd9a584d57e39cc6e23c9e3
Revant Kulkarni <75092585+revantKulkarni@users.noreply.github.com>
2021-09-11 21:01:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="side-bar">
+        <div className="side-bar__content-top">
+          <p className="pillar-name">AM Packaging</p>
+
+          <div className="container">
+            <img
+              className="svg"
+              alt=""
+              src="data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M5.51256 1.99478C6.30117 1.66813 7.14641 1.5 8 1.5C8.85359 1.5 9.69883 1.66813 10.4874 1.99478C11.2761 2.32144 11.9926 2.80023 12.5962 3.40381C13.1998 4.00739 13.6786 4.72394 14.0052 5.51256C14.3319 6.30117 14.5 7.14641 14.5 8C14.5 9.72391 13.8152 11.3772 12.5962 12.5962C11.3772 13.8152 9.72391 14.5 8 14.5C7.14641 14.5 6.30117 14.3319 5.51256 14.0052C4.72394 13.6786 4.00739 13.1998 3.40381 12.5962C2.18482 11.3772 1.5 9.72391 1.5 8C1.5 6.27609 2.18482 4.62279 3.40381 3.40381C4.00739 2.80023 4.72394 2.32144 5.51256 1.99478Z' fill='%230E7B3B' stroke='%23F9F9F9' stroke-width='3'/%3E%3C/svg%3E%0A"
+            ></img>
+            <p>Self assessment</p>
+          </div>
+
+          <div className="container">
+            <img
+              className="svg"
+              alt=""
+              src="data:image/svg+xml,%3Csvg width='15' height='16' viewBox='0 0 15 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11.2 6.4H3.2V4.8H11.2V6.4ZM11.2 9.6H3.2V8H11.2V9.6ZM8.8 12.8H3.2V11.2H8.8V12.8ZM7.2 1.6C7.41217 1.6 7.61566 1.68429 7.76569 1.83431C7.91571 1.98434 8 2.18783 8 2.4C8 2.61217 7.91571 2.81566 7.76569 2.96569C7.61566 3.11571 7.41217 3.2 7.2 3.2C6.98783 3.2 6.78434 3.11571 6.63432 2.96569C6.48429 2.81566 6.4 2.61217 6.4 2.4C6.4 2.18783 6.48429 1.98434 6.63432 1.83431C6.78434 1.68429 6.98783 1.6 7.2 1.6ZM12.8 1.6H9.456C9.12 0.672 8.24 0 7.2 0C6.16 0 5.28 0.672 4.944 1.6H1.6C1.17565 1.6 0.768687 1.76857 0.468629 2.06863C0.168571 2.36869 0 2.77565 0 3.2V14.4C0 14.8243 0.168571 15.2313 0.468629 15.5314C0.768687 15.8314 1.17565 16 1.6 16H12.8C13.2243 16 13.6313 15.8314 13.9314 15.5314C14.2314 15.2313 14.4 14.8243 14.4 14.4V3.2C14.4 2.77565 14.2314 2.36869 13.9314 2.06863C13.6313 1.76857 13.2243 1.6 12.8 1.6Z' fill='%23333333'/%3E%3C/svg%3E%0A"
+            ></img>
+            <p>AD00.329.87</p>
+          </div>
+          <div className="container">
+            <img
+              className="svg"
+              alt=""
+              src="data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.6 12.8V14.4H4.8V12.8H1.6ZM1.6 9.6V11.2H9.6V9.6H1.6ZM6.4 12.8V14.4H9.6V12.8H6.4ZM11.2 9.6V11.2H14.4V9.6H11.2ZM11.2 12.8V14.4H14.4V12.8H11.2ZM0 16V4.8L4 8V4.8L8 8V4.8L12 8L12.8 0H15.2L16 8V16H0Z' fill='%23333333'/%3E%3C/svg%3E%0A"
+            ></img>
+            <p>Amsterdam</p>
+          </div>
+          <div className="container">
+            <img
+              className="svg"
+              alt=""
+              src="data:image/svg+xml,%3Csvg width='15' height='16' viewBox='0 0 15 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M3.2 7.2H7.2V11.2H3.2V7.2ZM12.8 14.4H1.6V5.6H12.8V14.4ZM12.8 1.6H12V0H10.4V1.6H4V0H2.4V1.6H1.6C0.712 1.6 0 2.32 0 3.2V14.4C0 14.8243 0.168571 15.2313 0.468629 15.5314C0.768687 15.8314 1.17565 16 1.6 16H12.8C13.2243 16 13.6313 15.8314 13.9314 15.5314C14.2314 15.2313 14.4 14.8243 14.4 14.4V3.2C14.4 2.77565 14.2314 2.36869 13.9314 2.06863C13.6313 1.76857 13.2243 1.6 12.8 1.6Z' fill='%23333333'/%3E%3C/svg%3E%0A"
+            ></img>
+            <p>Rel 22-03-2021</p>
+          </div>
+          <div className="container">
+            <img
+              className="svg"
+              alt=""
+              src="data:image/svg+xml,%3Csvg width='13' height='14' viewBox='0 0 13 14' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M6.4 0.599976C7.24869 0.599976 8.06262 0.937117 8.66274 1.53723C9.26286 2.13735 9.6 2.95128 9.6 3.79998C9.6 4.64867 9.26286 5.4626 8.66274 6.06272C8.06262 6.66283 7.24869 6.99998 6.4 6.99998C5.55131 6.99998 4.73737 6.66283 4.13726 6.06272C3.53714 5.4626 3.2 4.64867 3.2 3.79998C3.2 2.95128 3.53714 2.13735 4.13726 1.53723C4.73737 0.937117 5.55131 0.599976 6.4 0.599976ZM6.4 8.59998C9.936 8.59998 12.8 10.032 12.8 11.8V13.4H0V11.8C0 10.032 2.864 8.59998 6.4 8.59998Z' fill='%23333333'/%3E%3C/svg%3E%0A"
+            ></img>
+            <p>John Doodle, Kate Mannister</p>
+          </div>
+
+          <hr className="seperator" />
+
+          <div className="container">
+            <img
+              className="svg"
+              alt=""
+              src="data:image/svg+xml,%3Csvg width='16' height='14' viewBox='0 0 16 14' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M6.4 13.8V8.99995H9.6V13.8H13.6V7.39995H16L8 0.199951L0 7.39995H2.4V13.8H6.4Z' fill='%23333333'/%3E%3C/svg%3E"
+            ></img>
+            <a className="home" href="www.google.com">
+              Go to Assessment Home
+            </a>
+          </div>
+          <div className="container">
+            <img
+              className="svg"
+              alt=""
+              src="data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11.7156 14.7556L9.92889 11.6622C10.8 11.0222 11.4044 10.0267 11.5289 8.88889H15.1111C14.96 11.3422 13.6444 13.4756 11.7156 14.7556ZM8.88889 3.58222V0C12.7111 0.231111 15.7689 3.28889 16 7.11111H12.4178C12.2133 5.25333 10.7467 3.78667 8.88889 3.58222ZM3.55556 8.44444C3.55556 9.01333 3.67111 9.55556 3.89333 10.0444L0.8 11.8311C0.284445 10.8089 0 9.66222 0 8.44444C0 4.41778 3.14667 1.12889 7.11111 0.888889V4.47111C5.11111 4.69333 3.55556 6.38222 3.55556 8.44444ZM7.55556 16C4.91556 16 2.59556 14.6667 1.24444 12.6044L4.33778 10.8178C5.06667 11.8044 6.22222 12.4444 7.55556 12.4444C8.12444 12.4444 8.66667 12.3289 9.15556 12.1067L10.9422 15.2C9.92 15.7156 8.77333 16 7.55556 16Z' fill='%23333333'/%3E%3C/svg%3E%0A"
+            ></img>
+            <a className="results" href="www.google.com">
+              Go to Results
+            </a>
+          </div>
+          <div className="container">
+            <img
+              className="svg"
+              alt=""
+              src="data:image/svg+xml,%3Csvg width='18' height='14' viewBox='0 0 18 14' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M17.5 1.70504L5.5 13.705L0 8.20504L1.41 6.79504L5.5 10.875L16.09 0.295044L17.5 1.70504Z' fill='%230E7B3B'/%3E%3C/svg%3E%0A"
+            ></img>
+            <p>Mark Pillar as done</p>
+          </div>
+
+          <hr className="seperator" />
+
+          <div className="side-bar__content-bottom">
+            <div className="pillar-content">Contents</div>
+            <p>Content x</p>
+            <p>Content x</p>
+            <p>Content x</p>
+            <p>Content x</p>
+            <p>Content x</p>
+            <p>Content x</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
